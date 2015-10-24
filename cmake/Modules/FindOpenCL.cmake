86878aa87154a1282b62592cd14b8dd39630c10b
Dominic Meiser <dmeiser@txcorp.com>
2015-10-23 20:38:42 -0600

    Install opencl drivers.

---
+++ cmake/Modules/FindOpenCL.cmake
@@ -0,0 +1,13 @@
+# Use OPENCL_ROOT_DIR to specify location of cgreen
+# When successful this defines
+# OPENCL_FOUND
+# OPENCL_LIB
+# OPENCL_INCLUDE_DIR
+
+find_path(OPENCL_INCLUDE_DIR CL/cl.h
+    HINTS ${OPENCL_ROOT_DIR}/include;/usr/include;/usr/local/include)
+find_library(OPENCL_LIB OpenCL
+    HINTS ${OPENCL_ROOT_DIR}/lib64;/usr/local/lib;/usr/lib;/usr/lib64)
+if (OPENCL_INCLUDE_DIR AND OPENCL_LIB)
+  set(OPENCL_FOUND TRUE)
+endif ()
